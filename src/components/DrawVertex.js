7ab15a49682659b31a87b668ea03225ff7247cb5
Wilson Chen <wilson20120311@gmail.com>
2021-08-31 18:17:06 +0800

    update vertax with connect when dragging

--- src/components/DrawVertex.js
+++ src/components/DrawVertex.js
@@ -13,78 +13,96 @@ import { PositionContext } from './common/PositionContext'
  * @param {Array} forwardedArrowsRefs [..., {current: dom} ] -- dom for all arrows svg  
  * @returns {JSX} div UI for one vertex 
  */
-export default function DrawVertex({name, column, row, forwardedRef, forwardedArrowsRefs, isDefaultGraph}) {
-    
-    let topPosition = 150 + 150 * row
-    let leftPosition = 150 * column
-    let topStyle = topPosition + 'px'
-    let leftStyle = leftPosition + 'px'
+export default function DrawVertex({ name, column, row, forwardedRef, forwardedArrowsRefs, isDefaultGraph, isDragged, setIsDragged }) {
 
-    const [isDisplaced, setIsDisplaced] = useState(false)
-    const [isMouseUp, setIsMouseUp] = useState(false)
-    let [positionMap, setPositionMap] = useContext(PositionContext)
-    const [translate, setTranslate] = useState({ x: 0, y: 0 })
-    const { drawConnectorDynamic } = useDrawConnector()
+  let topPosition = 150 + 150 * row
+  let leftPosition = 200 * column
+  let topStyle = topPosition + 'px'
+  let leftStyle = leftPosition + 'px'
 
-    const handleDrag = (newX, newY) => {
-        setTranslate({
-            x: newX,
-            y: newY
-        })
-        let relatedArrows = relatedArrowsDetector(forwardedArrowsRefs, name)
-        drawConnectorDynamic(relatedArrows, name, forwardedRef.current, translate) // but translate not new one? 
+  const [isDisplaced, setIsDisplaced] = useState(false)
+  const [isMouseUp, setIsMouseUp] = useState(false)
+  let [positionMap, setPositionMap] = useContext(PositionContext)
+  const [translate, setTranslate] = useState({ x: 0, y: 0 })
+  const { drawConnectorDynamic } = useDrawConnector()
+
+  const handleDrag = (newX, newY) => {
+    console.log({
+      x: newX,
+      y: newY
+    })
+    setTranslate({
+      x: newX,
+      y: newY
+    })
+    setIsDragged(true)
+    let relatedArrows = relatedArrowsDetector(forwardedArrowsRefs, name)
+    drawConnectorDynamic(relatedArrows, name, forwardedRef.current, translate) // but translate not new one? 
+  }
+
+  const { isDragging } = useDrag(forwardedRef, [translate], {
+    onDrag: handleDrag,
+    onPointerUp: () => setIsMouseUp(!isMouseUp),
+    onPointerDown: () => {
+      setIsDisplaced(true)
     }
+  })
 
-    const { isDragging } = useDrag(forwardedRef, [translate], {
-        onDrag: handleDrag,
-        onPointerUp: () => setIsMouseUp(!isMouseUp),
-        onPointerDown: () => {
-          setIsDisplaced(true)
-        }
-    }) 
+  let style =
+    isDefaultGraph
+      ? {
+        position: `absolute`, top: `${topStyle}`, left: `${leftStyle}`,
+        transform: `translateX(${translate.x}px) translateY(${translate.y}px)`
+      }
+      : {
+        position: `absolute`, top: `${topStyle}`, left: `${leftStyle}`,
+        transform: `translateX(${translate.x}px) translateY(${translate.y}px)`
+      }
 
-    let style = 
-      isDefaultGraph
-      ? { position:`absolute`, top: `${topStyle}`, left: `${leftStyle}` }
-      : { position:`absolute`, top: `${topStyle}`, left: `${leftStyle}`,
-          transform: `translateX(${translate.x}px) translateY(${translate.y}px)` }
+  useEffect(() => {
+    // 更新 div 剛創建、以及拖曳後 的位置狀態到 PositoinContext
+    // Dependency: 拖曳時，以及按了按鈕使切換預設/拖曳位置時⋯⋯兩個情況都會更新 context 
 
-    useEffect(() => {
-      // 更新 div 剛創建、以及拖曳後 的位置狀態到 PositoinContext
-      // Dependency: 拖曳時，以及按了按鈕使切換預設/拖曳位置時⋯⋯兩個情況都會更新 context 
-      
-      setPositionMap(prevState => ({
-        ...prevState,
-        [name]: {
-          isDisplaced: isDisplaced,
-          positionOrigin: [topPosition, leftPosition],
-          positionNew: isDefaultGraph ? [topPosition, leftPosition] : [topPosition + translate.x, leftPosition + translate.y],
-          translate: isDefaultGraph ? {x:0, y:0} : translate
-        }
-      }))
-      
-      console.group("Child -- DrawVertex")
-      console.log(positionMap) 
-      console.groupEnd()
-    }, [isDefaultGraph, isMouseUp])
+    // 點擊後會恢復成原點
+    if (isDefaultGraph && !isDragged) {
+      // if (isDefaultGraph && (!topStyle || !leftStyle)) {
+      setTranslate({
+        x: 0,
+        y: 0
+      })
+    }
+    setPositionMap(prevState => ({
+      ...prevState,
+      [name]: {
+        isDisplaced: isDisplaced,
+        positionOrigin: [topPosition, leftPosition],
+        positionNew: isDefaultGraph ? [topPosition, leftPosition] : [topPosition + translate.x, leftPosition + translate.y],
+        translate: isDefaultGraph ? { x: 0, y: 0 } : translate
+      }
+    }))
 
-    return (
-        <div 
-            ref={forwardedRef}
-            style={style} 
-            className="vertex"
-            id={name} >
-              {isDragging ? `${name} is now 🚀` : name}
-        </div>
-    )
-    
+    console.group("Child -- DrawVertex")
+    console.log(positionMap)
+    console.groupEnd()
+  }, [isDefaultGraph, isMouseUp])
+
+  return (
+    <div
+      ref={forwardedRef}
+      style={style}
+      className="vertex"
+      id={name} >
+      {isDragging ? `${name} is now 🚀` : name}
+    </div>
+  )
+
 }
 
 // 不能 Hoist，因為要等 arrow 畫出來才有辦法存取到  arrows refs
 const relatedArrowsDetector = (forwardedArrowsRefs, vertexName) => {
-    let relatedArrows = forwardedArrowsRefs.filter((arrow) => {
-        arrow = arrow.current
-        return arrow && (arrow.getAttribute("vertex_from") === vertexName || arrow.getAttribute("vertex_to") === vertexName)
-    })
-    return relatedArrows
+  let relatedArrows = forwardedArrowsRefs.filter((arrow) => {
+    arrow = arrow.current
+    return arrow && (arrow.getAttribute("vertex_from") === vertexName || arrow.getAttribute("vertex_to") === vertexName)
+  })
+  return relatedArrows
 }
